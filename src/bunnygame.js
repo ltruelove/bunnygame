85fc23cb8cca04e561cf52084b67faa621f7182d
unknown <ltruelove@LTRUELOVE.npipm.local>
2014-01-02 13:21:24 -0500

    Added box2dweb library and used it on the box that was suspended in the air previously.

--- src/bunnygame.js
+++ src/bunnygame.js
@@ -1,7 +1,11 @@
+var SCALE = 30;
+
 function BunnyGame(){
 	// create an new instance of a pixi stage
 	this.stage = new PIXI.Stage(0x66FF99);
-	
+    this.world = new Object();
+	this.setupPhysics();
+    
 	// create a renderer instance
 	this.renderer = new PIXI.CanvasRenderer(400, 300);
 	
@@ -16,16 +20,33 @@ function BunnyGame(){
 	
 	// create a texture from an image path
 	var blockTexture = PIXI.Texture.fromImage("resources/block.png");
+    
+    this.blockView = new Object();
 	// create a new Sprite using the texture
-	var block = new PIXI.Sprite(blockTexture);
+	this.blockView.block = new PIXI.Sprite(blockTexture);
 	
 	//position the block sprite
-	block.position.x = 150;
-	block.position.y = 67;
-	
+	this.blockView.block.position.x = 150;
+	this.blockView.block.position.y = 67;
+    
+    var fixDef = new box2d.b2FixtureDef();
+    fixDef.density = 1;
+    fixDef.friction = 0.5;
+    
+    var bodyDef = new box2d.b2BodyDef();
+    bodyDef.type = box2d.b2Body.b2_dynamicBody;
+    bodyDef.position.x = this.blockView.block.position.x / SCALE;
+    bodyDef.position.y = this.blockView.block.position.y / SCALE;
+    
+    fixDef.shape = new box2d.b2PolygonShape();
+    fixDef.shape.SetAsBox(200 / SCALE, 20 / SCALE);
+    
+    this.blockView.body = this.world.CreateBody(bodyDef);
+    this.blockView.body.CreateFixture(fixDef);
+    
 	this.stage.addChild(this.back);
 	this.stage.addChild(this.fore);
-	this.stage.addChild(block);
+	this.stage.addChild(this.blockView.block);
 	this.stage.addChild(this.bunny);
 	
 	document.onkeydown = this.keydown.bind(this);
@@ -36,6 +57,25 @@ function BunnyGame(){
 }
 
 BunnyGame.constructor = BunnyGame;
+
+BunnyGame.prototype.setupPhysics = function(){
+    this.world = new box2d.b2World(new box2d.b2Vec2(0,50),true);
+    
+    //create ground
+    var fixDef = new box2d.b2FixtureDef();
+    fixDef.density = 1;
+    fixDef.friction = 0.5;
+    
+    var bodyDef = new box2d.b2BodyDef();
+    bodyDef.type = box2d.b2Body.b2_staticBody;
+    bodyDef.position.x = 200 / SCALE;
+    bodyDef.position.y = 300 / SCALE;
+    
+    fixDef.shape = new box2d.b2PolygonShape();
+    fixDef.shape.SetAsBox(200 / SCALE, 20 / SCALE);
+    
+    this.world.CreateBody(bodyDef).CreateFixture(fixDef);
+}
 
 BunnyGame.prototype.keydown = function(evt){
 	if(this.keysPressed.indexOf(evt.keyCode) == -1){
@@ -61,12 +101,16 @@ BunnyGame.prototype.update = function(){
 	    // just for fun, lets rotate mr rabbit a little
 	    //bunny.rotation += 0.1;
 		
+        this.blockView.block.position.x = this.blockView.body.GetPosition().x * SCALE;
+        this.blockView.block.position.y = this.blockView.body.GetPosition().y * SCALE;
 		this.bunny.update();
 		this.back.update();
 		this.fore.update();
 		
 	    // render the stage   
 	    this.renderer.render(this.stage);
+        this.world.Step(1/60, 10, 10);
+        this.world.ClearForces();
 		
 	    requestAnimFrame( this.update.bind(this) );
 }

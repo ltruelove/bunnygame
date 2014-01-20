2dfbcac513972c849d94e0699683ea1499ce5452
Levi Truelove <levi@levitruelove.com>
2014-01-19 22:56:35 -0500

    Playing around with some Tiled files. I can get the platformer type objects working, but if I try to use an object layer that lets me specify collision areas it's not working. Also I created a very large level and that didn't appear to want to load. Might have to do that as multiple maps or layers. Not sure yet.

--- src/game.js
+++ src/game.js
@@ -1,4 +1,9 @@
 var bunnySprite;
+var map;
+var tileset;
+var layer;
+var cursors; 
+
 var game = new Phaser.Game(800,600, Phaser.Auto, '', {
     preload: preload,
     create: create,
@@ -7,25 +12,42 @@ var game = new Phaser.Game(800,600, Phaser.Auto, '', {
 
 function preload(){
     game.load.image('bunny', '/resources/bunny.png');
+    game.load.tilemap("platforms", "/resources/platforms.json", null, Phaser.Tilemap.TILED_JSON);
+    game.load.tileset("land", "/resources/land.png", 32, 32);
 }
 
 function create(){
-    bunnySprite = game.add.sprite(game.world.centerX, 200, 'bunny');
-    bunnySprite.body.acceleration.y = 500;
-    bunnySprite.body.collideWorldBounds = true;
-    bunnySprite.body.drag.x = 500;
+    map = game.add.tilemap("platforms");
+    tileset = game.add.tileset("land");
+    tileset.setCollisionRange(0, tileset.total-1, true, true, true, true);
+    // now we need to create a game layer, and assign it a tile set and a map
+    layer = game.add.tilemapLayer(0, 0, 800, 600, tileset, map, 0);
+
+    bunnySprite = game.add.sprite(10, 500, 'bunny');
+    bunnySprite.body.gravity.y = 8;
+    //bunnySprite.body.collideWorldBounds = true;
+    //bunnySprite.body.drag.x = 500;
+
+    // the fastest way to create game controls is "createCursorKeys" method
+    // which automatically assigns up, down, left and right movement to
+    // arrow keys
+    cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update(){
-    if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-        bunnySprite.body.velocity.x = -200;
+    game.physics.collide(bunnySprite, layer)
+    bunnySprite.body.velocity.x = 0;
+    
+    // are we moving left?
+    if (cursors.left.isDown){
+        bunnySprite.body.velocity.x = -100;
     }
-
-    if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-        bunnySprite.body.velocity.x = 200;
+    // are we moving right?
+    if (cursors.right.isDown){
+        bunnySprite.body.velocity.x = 100;
     }
-
-    if(game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
-        bunnySprite.body.velocity.y = -100;
+    // are we jumping? 
+    if (cursors.up.isDown && bunnySprite.body.touching.down){
+        bunnySprite.body.velocity.y = -300;
     }
 }

00cad53dfd57e4a0fce9145c881c838f6e42a35a
Levi Truelove <levi@levitruelove.com>
2014-02-07 15:07:05 -0500

    Added a second layer to the level. Getting ready to try making it work with the nadion library.

--- src/BunGame.js
+++ src/BunGame.js
@@ -17,21 +17,21 @@ MainGame.BunnyGame.prototype = {
         this.game.load.tilemap("platforms", "/resources/level1.json", null, Phaser.Tilemap.TILED_JSON);
         this.game.load.tileset("land", "/resources/tiles_spritesheet.png", this.tileWidth, this.tileHeight,144,0,1);
         //this.game.load.image('L1BG', 'resources/L1BG.png');
-        game.load.audio('music', ['/resources/L1Audio.mp3']);
+        //game.load.audio('music', ['/resources/L1Audio.mp3']);
     },
     
     create: function() {
         this.map = this.game.add.tilemap("platforms");
-        this.game.stage.backgroundColor = '#FFF';
+        //this.game.stage.backgroundColor = '#000';
         //background = this.game.add.tileSprite(0, 0, 1400, 3500, "L1BG");
         this.tileset = this.game.add.tileset("land");
-        this.tileset.spacing = 1;
+        console.log(this.bgtileset);
         this.tileset.setCollisionRange(0, this.tileset.total-1, true, true, true, true);
     
         //add a background tile layer
-        //this.bglayer = this.game.add.tilemapLayer(0, 0, 800, 600, this.tileset, this.map, 0);
+        this.bglayer = this.game.add.tilemapLayer(0, 0, 800, 600, this.tileset, this.map, 0);
         // now we need to create a game layer, and assign it a tile set and a map
-        this.layer = this.game.add.tilemapLayer(0, 0, 800, 600, this.tileset, this.map, 0);
+        this.layer = this.game.add.tilemapLayer(0, 0, 800, 600, this.tileset, this.map, 1);
     
         //this.music = game.add.audio('music');
         //this.music.play();
@@ -41,12 +41,12 @@ MainGame.BunnyGame.prototype = {
         // Set Anchor to the center of your sprite
         this.bunnySprite.anchor.setTo(.5,1);
 
-        this.bunnySprite.body.gravity.y = 15;
+        this.bunnySprite.body.gravity.y = 20;
         // I'm not so sure we need this one.
         this.bunnySprite.body.collideWorldBounds = true;
     
         this.game.world.setBounds(0,0,this.tilesWide*this.tileWidth,
-        this.tilesHigh*this.tileHeight); //setting the bounds of the entire level
+            this.tilesHigh*this.tileHeight); //setting the bounds of the entire level
         this.game.camera.follow(this.bunnySprite); //bounds lets us set the camera to follow the character
         this.cursors = this.game.input.keyboard.createCursorKeys();
     },
@@ -71,7 +71,7 @@ MainGame.BunnyGame.prototype = {
 
         // are we jumping? 
         if (this.cursors.up.isDown && this.bunnySprite.body.touching.down){
-            this.bunnySprite.body.velocity.y = -750;
+            this.bunnySprite.body.velocity.y = -850;
             this.bunnySprite.animations.stop('walk');
         }
         if(this.bunnySprite.body.velocity.x == 0 || !this.bunnySprite.body.touching.down){

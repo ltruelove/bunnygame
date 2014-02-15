41f3a269e191fc286ce7e6903b27dfcd7b7010c4
Levi Truelove <levi@levitruelove.com>
2014-02-15 15:35:32 -0500

    Animated the test slime enemy and added some movement logic. It will move left until it hits the left end of the world. When it hits, it turns around and moves right until it hits the right end of the world and repeats.

--- src/BunGame.js
+++ src/BunGame.js
@@ -17,6 +17,8 @@ MainGame.BunnyGame = function(game) {
     this.jumpButton = null;
     this.walkFrames = null;
     this.slimeWalkFrames = null;
+    this.slimeLeft = true;
+    this.playerAnimFrames = 15;
 }
 
 MainGame.BunnyGame.prototype = {
@@ -45,11 +47,14 @@ MainGame.BunnyGame.prototype = {
     
         //this.music = game.add.audio('music');
         //this.music.play();
-        this.testSlime = this.game.add.sprite(300, 3100, 'enemies',12);
+
+        //add a test enemy
+        this.testSlime = this.game.add.sprite(100, 3400, 'enemies',12);
         this.testSlime.body.gravity.y = 15;
         this.testSlime.body.collideWorldBounds = true;
-        this.slimeWalkFrames = Phaser.Animation.generateFrameNames('slime_walk', 1, 2, '', 0);
-        //this.testSlime.animations.add('slimewalk',this.slimeWalkFrames,20,true,false);
+        this.slimeWalkFrames = Phaser.Animation.generateFrameNames('slimeWalk', 1, 2, '', 0);
+        this.testSlime.animations.add('slimewalk',this.slimeWalkFrames,5,true,false);
+        this.testSlime.anchor.setTo(.5,1);
 
         this.bunnySprite = this.game.add.sprite(10, 3400, 'alien');
         this.walkFrames = Phaser.Animation.generateFrameNames('p3_walk', 1, 11, '', 1);
@@ -76,18 +81,47 @@ MainGame.BunnyGame.prototype = {
     },
     
     update: function(){
+        //make the player collide with the world
         this.game.physics.collide(this.bunnySprite, this.layer);
+
+        //make the test enemy collide with the world
         this.game.physics.collide(this.testSlime, this.layer);
+
+        //handle the collision of the player and the goal
         this.game.physics.collide(this.bunnySprite, this.goalSprite, this.goalCollision, null, this);
+
+        //reset velocities
         this.bunnySprite.body.velocity.x = 0;
-        //this.testSlime.animations.play('slimewalk',20,true);
+        this.testSlime.body.velocity.x = 0;
+        
+        //animate the test enemy
+        if(this.testSlime.body.touching.down){
+            if(this.testSlime.position.x - (this.testSlime.body.halfWidth) <= this.game.world.bounds.x){
+                this.slimeLeft = false;
+            }
+
+            if(this.testSlime.position.x >= this.game.world.bounds.right - (this.testSlime.body.halfWidth + 1)){
+                this.slimeLeft = true;
+            }
+
+            if(this.slimeLeft == true){
+                //test enemy moves left
+                this.testSlime.body.velocity.x = -50;
+                this.testSlime.scale.x = +1;
+            }else{
+                //test enemy moves right
+                this.testSlime.body.velocity.x = +50;
+                this.testSlime.scale.x = -1;
+            }
+        }
+        this.testSlime.animations.play('slimewalk',5,true);
         
         // are we moving left?
         if (this.cursors.left.isDown){
             this.bunnySprite.body.velocity.x = -200;
             // Invert scale.x to flip left/right
             this.bunnySprite.scale.x = -1;
-            this.bunnySprite.animations.play('walk',20,true);
+            this.bunnySprite.animations.play('walk',this.playerAnimFrames,true);
             if(this.bunnySprite.position.x > 0 && this.game.camera.view.x > 0){
                 this.background.tilePosition.x += .1;
             }
@@ -96,7 +130,7 @@ MainGame.BunnyGame.prototype = {
         if (this.cursors.right.isDown){
             this.bunnySprite.body.velocity.x = 200;
             this.bunnySprite.scale.x = 1;
-            this.bunnySprite.animations.play('walk',20,true);
+            this.bunnySprite.animations.play('walk',this.playerAnimFrames,true);
             if(this.bunnySprite.position.x < 
                (this.tileWidth * this.tilesWide - this.bunnySprite.width)){
                 this.background.tilePosition.x -= .1;
@@ -106,20 +140,20 @@ MainGame.BunnyGame.prototype = {
         //standing still
         if(this.bunnySprite.body.velocity.x == 0){
             this.bunnySprite.animations.stop('walk');
-            this.bunnySprite.animations.play('stand',20,true);
+            this.bunnySprite.animations.play('stand',this.playerAnimFrames,true);
         }
 
         //did we press the jump key?
         if (this.cursors.up.isDown && this.bunnySprite.body.touching.down){
             this.bunnySprite.body.velocity.y = -750;
             this.bunnySprite.animations.stop('walk');
-            this.bunnySprite.animations.play('jump',20,true);
+            this.bunnySprite.animations.play('jump',this.playerAnimFrames,true);
         }
 
         // are we in the air? 
         if(!this.bunnySprite.body.touching.down){
             this.bunnySprite.animations.stop('walk');
-            this.bunnySprite.animations.play('jump',20,true);
+            this.bunnySprite.animations.play('jump',this.playerAnimFrames,true);
         }
     },
 
